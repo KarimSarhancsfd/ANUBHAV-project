18085b0d8fcd661d686224389862f41468eb94b0
karimsarhan <kimokomono74@gmail.com>
2026-02-24 15:27:13 +0200

    ressolving some errors

--- anubhav-back/src/app.module.ts
+++ anubhav-back/src/app.module.ts
@@ -1,5 +1,6 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { CommonModule } from './common/common.module';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -25,6 +26,7 @@ import { EconomyModule } from './economy/economy.module';
 
 @Module({
   imports: [
+    CommonModule,
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot({
       type: 'mysql',
